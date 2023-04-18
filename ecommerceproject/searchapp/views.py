5da1d7fa5e6a241363b77eacf028310508823e57
Megharaj <megharajmkdy97@gmail.com>
2023-04-18 16:26:19 +0530

    added

---
+++ ecommerceproject/searchapp/views.py
@@ -0,0 +1,12 @@
+from Kyra.models import Product
+from django.db.models import Q
+from django.shortcuts import render
+
+
+def SearchResult(request):
+    products=None
+    query=None
+    if 'q' in request.GET:
+        query=request.GET.get('q')
+        products=Product.objects.all().filter(Q(name__contains=query) | Q(desc__contains=query))
+    return render(request,'search.html',{'query':query,'products':products})
